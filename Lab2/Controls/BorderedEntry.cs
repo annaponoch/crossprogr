dfa84b541a1fceac5a54874222497ad17fbd78e0
annaponoch <60112879+annaponoch@users.noreply.github.com>
2023-06-06 13:12:39 +0300

    Delete Lab2 directory

--- Lab2/Controls/BorderedEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Maui.Controls;
-namespace Lab2.Controls
-{
-    public class BorderedEntry : Entry
-    {
-        // You can add custom properties, methods, and events here
-    }
-}
-
--- Lab2/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Lab2;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
